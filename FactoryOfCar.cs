2488ccdd2918910ce55d8ff7c40aaac701a43d3f
Gev123 <gevorg.mkrtchyan@milies.net>
2021-11-04 18:45:31 +0400

    Initial commit

---
+++ FactoryOfCar.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001DelegateApp
+{
+    public class FactoryOfCar
+    {
+        public delegate void CarHandler(string message);
+        CarHandler _carHandler;
+        public void RegisterHandler(CarHandler carHandler)
+        {
+            _carHandler = carHandler;
+        }
+
+        int _count;
+        string CarName { get; set; }
+        string CarColor { get; set; }
+        public FactoryOfCar(int count,string carName,string carColor)
+        {
+            _count = count;
+            CarColor = carColor;
+            CarName = carName;
+        }
+        public int CurrentCount
+        {
+            get { return _count; }
+        }
+        public void Add(int count,string carName,string carColor)
+        {
+            if (carName!=null && carColor!=null)
+            {
+                _count += count;
+            }
+        }
+        public void Sell(int count)
+        {
+            if (count<=_count)
+            {
+                _count -= count;
+                if (_carHandler!=null)
+                {
+                    _carHandler($"The number of cars sold is : {count}");
+                }
+            }
+            else
+            {
+                if (_carHandler!=null)
+                {
+                    _carHandler($"There is no car in warehouse");
+                }
+            }
+        }
+        
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001DelegateApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FactoryOfCar car = new FactoryOfCar(5, "Sedan", "Red");
+            car.RegisterHandler(new FactoryOfCar.CarHandler(Show));
+            car.Sell(4);
+            car.Sell(2);
+            car.Sell(1);
+            Console.ReadLine();
+        }
+
+        private static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
